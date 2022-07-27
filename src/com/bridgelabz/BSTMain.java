d1a33d093fcecdb0db9b5b7823a035bbae7b92ea
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-27 16:35:06 +0530

    Refactor UC-1

--- src/com/bridgelabz/BSTMain.java
+++ src/com/bridgelabz/BSTMain.java
@@ -4,10 +4,25 @@ public class BSTMain {
 
 	public static void main(String[] args) {	
 			BinarySearchTree tree = new BinarySearchTree();
-
+			//Adding Root
 			tree.insert(56);
+			//Nodes just below root
 			tree.insert(30);
 			tree.insert(70);
+			//Adding Child nodes for 30 and 70
+			tree.insert(22);
+			tree.insert(40);
+			tree.insert(60);
+			tree.insert(95);
+			//Adding Child nodes for 11 and 65
+			tree.insert(11);
+			tree.insert(65);
+			//Adding leaf nodes
+			tree.insert(3);
+			tree.insert(16);
+			tree.insert(63);
+			tree.insert(67);
+
 			// print inorder traversal of the BST
 			tree.inorder();
 	}
